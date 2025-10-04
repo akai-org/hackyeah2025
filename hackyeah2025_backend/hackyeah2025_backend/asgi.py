877f157cafad2db92935fda691b0e5e458e5f336
m-olej <music.olejniczak@wp.pl>
2025-10-05 01:00:41 +0200

    branch migration

--- hackyeah2025_backend/hackyeah2025_backend/asgi.py
+++ hackyeah2025_backend/hackyeah2025_backend/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import rt_geo.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackyeah2025_backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackyeah2025_backend.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(rt_geo.routing.websocket_urlpatterns)
+        ),
+    }
+)
